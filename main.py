bfc0758017b44ff6fe48d5862fbf4ead1d146aad
yatharth kapadia <yatharth.kapadia.1702@gmail.com>
2022-02-07 03:30:17 +0530

    file configured for cpu

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ import uvicorn
 ckpt = 'Narrativa/byt5-base-tweet-hate-detection'
 
 tokenizer = AutoTokenizer.from_pretrained(ckpt)
-model = T5ForConditionalGeneration.from_pretrained(ckpt).to("cuda")
+model = T5ForConditionalGeneration.from_pretrained(ckpt).to("cpu")
 
 def classify_sentence(sentence):
     
     inputs = tokenizer([sentence], padding='max_length', truncation=True, max_length=512, return_tensors='pt')
-    input_ids = inputs.input_ids.to('cuda')
-    attention_mask = inputs.attention_mask.to('cuda')
+    input_ids = inputs.input_ids.to('cpu')
+    attention_mask = inputs.attention_mask.to('cpu')
     output = model.generate(input_ids, attention_mask=attention_mask)
     return tokenizer.decode(output[0], skip_special_tokens=True)
 
